b465a521ded34239e7b2b157eb2b3db52099054e
colemonnahan <monnahc@uw.edu>
2018-05-24 13:09:37 -0400

    Update demo to make some example plots

--- inst/demo.R
+++ inst/demo.R
@@ -58,7 +58,7 @@ AllYears=seq(1950,2015)
 ###########################
 ## choose stock
 ###########################
-iStock <- 1
+iStock <- 4
 jStock <- which(CStock==StockList[iStock])
 ## data inputs for chosen stock
 Catch <- as.numeric(CatchData[jStock,])
@@ -79,29 +79,36 @@ draws <- draw.priors(nrep, iStock)
 ## Run SIR to get posterior samples
 fit1 <- run.SIR(draws, deplete.mean=FinalDepleteBest, deplete.cv=FinalDepleteCV)
 fit2 <- run.SIR(draws, deplete.mean=FinalDepleteBest, deplete.cv=FinalDepleteCV/10)
-fit3 <- run.SIR(draws, harvest.mean=.5, harvest.sd=.1)
+fit3 <- run.SIR(draws, deplete.mean=FinalDepleteBest,
+                deplete.cv=FinalDepleteCV, harvest.mean=.5, harvest.sd=.01)
 
 ## Quick exploratory plots
-par(mfcol=c(3,2))
+png(paste0("fits_", iStock, ".png"), width=9, height=6, units='in', res=300)
+par(mfcol=c(3,3), mar=c(.1,4,2,.5), oma=c(3, 1, 3, .5))
 ## Plot depletion trajectories
-for(i in 1:2){
-  if(i==1) temp=fit1 else temp=fit2
-plot(Years, Years, ylim=c(0,2),type="n",xlab="Year",
-     ylab="Depletion",main=StockList[iStock])
-trash <- apply(temp$depletion, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
-abline(h=FinalDepleteBest,col="red",lwd=3)
-## Same but for biomass
-plot(Years,Years, ylim=c(0,1.1*max(temp$ssb)), type="n",xlab="Year",
-     ylab="Vulnerable Biomass",main=StockList[iStock])
-trash <- apply(temp$ssb, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
-## Same but for harvest rate (U)
-plot(Years,Years, ylim=c(0,1.1*max(temp$U)), type="n",xlab="Year",
-     ylab="Harvest rate (U)",main=StockList[iStock])
-trash <- apply(temp$U, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
+fits <- list(fit1, fit2, fit3)
+for(i in 1:3){
+  temp <- fits[[i]]
+  ## use consistent SSB ylim
+  ylim <- c(0,1.05*max(unlist(lapply(fits, function(x) x$ssb))))
+  labels <- c("Standard", "Very informative depletion", "Very informative harvest")
+  plot(Years, Years, ylim=c(0,2),type="n",xlab=NA,
+       ylab="Depletion",main=labels[i])
+  trash <- apply(temp$depletion, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
+  abline(h=FinalDepleteBest,col="red",lwd=3)
+  ## Same but for biomass
+  plot(Years,Years, ylim=ylim, type="n",xlab=NA,
+       ylab="Vulnerable Biomass")
+  trash <- apply(temp$ssb, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
+  ## Same but for harvest rate (U)
+  plot(Years,Years, ylim=c(0,1), type="n",xlab="Year",
+       ylab="Harvest rate (U)")
+  trash <- apply(temp$U, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
+  mtext(StockList[iStock], outer=TRUE, line=1)
 }
-
+dev.off()
 ## Pairs plot of parameters priors and posterior
-dev.new()
-post.ind <- 1:ncol(temp$depletion) %in% temp$Keepers # which prior draws were "kept"
-pairs(draws[,1:5], col=ifelse(post.ind, 'red', 'black'),
-      cex=ifelse(post.ind, 1, .1))
+
+## post.ind <- 1:ncol(temp$depletion) %in% temp$Keepers # which prior draws were "kept"
+## pairs(draws[,1:5], col=ifelse(post.ind, 'red', 'black'),
+##       cex=ifelse(post.ind, 1, .1))
