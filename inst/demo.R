54ea3c3aecb6c8c6c88488d01fd070e38ba3a5ca
colemonnahan <monnahc@uw.edu>
2018-06-13 15:38:39 -0400

    Rework demo file to use new framework

--- inst/demo.R
+++ inst/demo.R
@@ -1,84 +1,28 @@
-## priors
-## invertebrates
-## figure out how far to push data sources on life history
-## figure out priors for depletion - change in effort
-	## know if species are caught by trawls?
-## swept-area ratio - fraction between area trawled and continental shelf - correlates with fishing mortality rate
-	## india - 10 - nothing long-lived is going to be around with that F
-
-## identify what type of species from databases
-## F prior
-## how to use FMI as depletion measure
-## relationship between trawl, effort, and F
-## species x in area Z but not species y in area Z
-
-### SHORT-TERM
-## extend FishLife for invertebrates
-## or use SeaLife/Base
-## go from FMI to level of depletion
-## FMI fishery score with more than 1 person
-## related to level of depletion, at Bmsy 0.8 or higher, linear down to zero
-## middle of April for invertebrates
-
-rm(list=ls())
+### This file provides a quick demonstration of the package using a
+### hard-coded example.
 library(FishLife)
 library(mvtnorm)
 library(dplyr)
 devtools::load_all('C:/Users/Cole/sraplus')
 library(sraplus)
+## You need to put the file "Return.RData" in the "data" folder of the
+## package. It's too large to include it and should be removed later. Comes
+## from FishLife
+data(Return)
+str(Return)
 
-###################
-## directories
-###################
-main_dir <- system.file(package='sraplus')
-setwd(main_dir)
-data_dir <- file.path(main_dir, "examples")
-
-###########################
-## data
-###########################
-load(file.path(data_dir, "Return.RData"))
-datafile <- file.path(data_dir, "Mexico FAO Catch Data.csv")
-priorfile <- file.path(data_dir, "DeplPrior_wTaxa.csv")
-
-## catch data
-C <- ReadCatchData(File=datafile)
-CatchData <- C$CatchData
-StockInfo <- C$StockInfo
-CStock <- paste(StockInfo$Species..ASFIS.species.,StockInfo$Fishing.area..FAO.major.fishing.area.)
-
-## prior info
-Priors <- ReadPrior(File=priorfile)
-StockList <- paste(Priors$Species..ASFIS.species.,Priors$Fishing.area..FAO.major.fishing.area.)
-
-## indices
-AllYears=seq(1950,2015)
-
-
-###########################
-## choose stock
-###########################
-iStock <- 4
-jStock <- which(CStock==StockList[iStock])
-# data inputs for chosen stock
-Catch <- as.numeric(CatchData[jStock,])
-Years <- AllYears[which(is.na(Catch)==FALSE)]
-Catch <- Catch[which(is.na(Catch)==FALSE)]
-NYears <- length(Years)
-NY <- length(Years)
-
-## final depletion prior
+## Hard-coded values for a specific stock
 CStock <- "Marine fishes nei Pacific, Eastern Central"
 Years <- 1956:2015
-FinalDepleteBest <- Priors$DepletePrior[iStock]
-FinalDepleteCV <- Priors$DepleteCV[iStock]
-Kprior <- Priors$Kprior[iStock]
-InitialDepletePrior <- Priors$InitialDepletePrior[iStock]
-InitialDepleteCV <- Priors$InitialDepleteCV[iStock]
-Taxon <- c(Class="Actinopterygii", Order="", Family="", Genus="", Species="")
-  Priors[iStock, c("Class", "Order", "Family", "Genus", "Species")]
+FinalDepleteBest <- 0.15
+FinalDepleteCV <- 0.2
+Kprior <- 5
+InitialDepletePrior <- 0.8
+InitialDepleteCV <- 0.2
+Taxon <- c(Class="Actinopterygii", Order="", Family="", Genus="",
+           Species="")
 Years <- 1950:2015
-NYears <- length(Years)
+NY <- length(Years)
 Catch <- c(11200, 11300, 8700, 10000, 5800, 3400, 5200, 2700, 19200, 16400,
            25600, 40000, 31800, 34000, 30000, 38400, 50000, 78000, 88100, 86500,
            91500, 111100, 117100, 122300, 18936, 25178, 40793, 2575, 97035, 157754,
@@ -88,14 +32,24 @@ Catch <- c(11200, 11300, 8700, 10000, 5800, 3400, 5200, 2700, 19200, 16400,
            46688, 48924, 47367, 78814, 18464, 22498, 32557)
 
 ## settings
-devtools::load_all('C:/Users/Cole/sraplus')
-library(sraplus)
 nrep <- 10000
 set.seed(2323)
 ## Draw from priors for SIR
 draws <- draw.priors(N=nrep, InitialDepletePrior, InitialDepleteCV,
                      Kprior, Catch, Taxon)
 ## Run SIR to get posterior samples
-fit1 <- run.SIR(NY=NY, Catch=Catch, draws=draws, deplete.mean=FinalDepleteBest,
+fit <- run.SIR(Catch=Catch, draws=draws, deplete.mean=FinalDepleteBest,
                 deplete.cv=FinalDepleteCV )
 
+## Quick plot to show the output
+par(mfrow=c(3,1))
+plot(Years, Years, ylim=c(0,2),type="n",xlab=NA,
+     ylab="Depletion",main=CStock)
+trash <- apply(fit$depletion, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
+plot(Years,Years, ylim= c(0,1.05*max(fit$ssb)), type="n",xlab=NA,
+     ylab="Vulnerable Biomass")
+trash <- apply(fit$ssb, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
+## Same but for harvest rate (U)
+plot(Years,Years, ylim=c(0,1), type="n",xlab="Year",
+     ylab="Harvest rate (U)")
+trash <- apply(fit$U, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
