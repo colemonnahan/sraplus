11cb986e3f70fe7015d1eced71e9c8760248ea05
colemonnahan <monnahc@uw.edu>
2018-07-05 17:18:03 -0400

    More updates to demo script

--- inst/demo.R
+++ inst/demo.R
@@ -9,7 +9,6 @@ library(sraplus)
 ## package. It's too large to include it and should be removed later. Comes
 ## from FishLife
 data(Return)
-str(Return)
 
 ## A simulated stock history for demonstration purposes
 
@@ -22,20 +21,20 @@ Taxon <- c(Class="Actinopterygii", Order="Perciformes",
 ## Draw from priors for SIR
 draws <- draw.priors(N=nrep, InitialDepletePrior=0.8,
                      InitialDepleteCV=0.2,
-                     Kprior=5, Catch=Catch, Taxon=Taxon)
+                     Kprior=3, Catch=Catch, Taxon=Taxon)
 ## Run SIR to get posterior samples
-fit <- run.SIR(Catch=Catch, draws=draws, deplete.mean=1,
+fit <- run.SIR(Catch=Catch, draws=draws, deplete.mean=2,
                 deplete.cv=0.5)
 
 ## Quick plot to show the output
 par(mfrow=c(3,1))
 Years <- 2005:2015
-plot(Years, Years, ylim=c(0,3),type="n",xlab=NA,
-     ylab="B/BMSY",main='Fake Tuna')
-trash <- apply(fit$bscaled, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
 plot(Years,Years, ylim= c(0,1.05*max(fit$ssb)), type="n",xlab=NA,
-     ylab="Vulnerable Biomass")
+     ylab="Vulnerable Biomass",main='Fake Tuna')
 trash <- apply(fit$ssb, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
+plot(Years, Years, ylim=c(0,4),type="n",xlab=NA,
+     ylab="B/BMSY")
+trash <- apply(fit$bscaled, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
 plot(Years,Years, ylim=c(0,2), type="n",xlab="Year",
      ylab="U/UMSY")
 trash <- apply(fit$uscaled, 2, function(i) lines(Years,y=i, col=rgb(0,0,0,.1)))
