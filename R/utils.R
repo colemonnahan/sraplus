7155d47b4ddabb12c71b3921cdb4d748378e0f2c
colemonnahan <monnahc@uw.edu>
2018-07-19 16:37:47 -0700

    Add plot_ssb with roxygen template

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,25 @@
 ### Utility functions for package
+
+#' Plot spawning biomass time series for posterior draws with catches.
+#' @template plot_args
+#' @param ylim The ylim to use, if not specified will be calculated
+#'   internally.
+#' @export
+plot_ssb <- function(fit, ylim=NULL){
+  if(is.null(fit$year)) fit$year <- 1:nrow(fit$ssb)
+  if(is.null(ylim)) ylim <- c(0, 1.05*max(fit$ssb))
+  plot(x=fit$year, y=fit$year, ylim=ylim, type="n",xlab='Year',
+       ylab="Vulnerable Biomass", main=NA)
+  ## Add individual trajectories that were kept
+  trash <- apply(fit$ssb, 2, function(i)
+    lines(fit$year, y=i, col=rgb(0,0,0,.1)))
+  ## Add catch
+  points(fit$year, fit$Catch, type='h', col='blue', lwd=3)
+}
+
 #' Plot the prior draws from a fit, showing which crashed the population
 #' and which were kept
-#' @param fit A returned list from run.SIR
-#' @return Nothing. A plot is created.
+#' @template plot_args
 #' @export
 plot_draws <- function(fit){
   n <- nrow(fit$draws)
@@ -17,10 +34,8 @@ plot_draws <- function(fit){
 
 #' Plot the realized recruitment deviations from a fit, showing which
 #' crashed the population and which were kept
-#' @param fit A returned list from run.SIR
-#' @return Nothing. A plot is created.
+#' @template plot_args
 #' @export
-#'
 plot_recdevs <- function(fit){
   n <- nrow(fit$draws)
   col <- rep('black', len=n)
