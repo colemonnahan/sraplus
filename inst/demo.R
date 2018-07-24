d36f193b94f87fc96436c5ab5c83a64d9c01ce96
colemonnahan <monnahc@uw.edu>
2018-07-23 17:10:09 -0700

    Update demo accordingly

--- inst/demo.R
+++ inst/demo.R
@@ -18,13 +18,9 @@ set.seed(2323)
 Catch <- runif(11, 50000, 300000)
 Taxon <- c(Class="Actinopterygii", Order="Perciformes",
            Family="Scombridae", Genus="Thunnus", Species="albacares")
-## Draw from priors for SIR
-draws <- draw.priors(N=nrep, InitialDepletePrior=0.8,
-                     InitialDepleteCV=0.2,
-                     Kprior=3, Catch=Catch, Taxon=Taxon)
 ## Run SIR to get posterior samples
-fit <- run.SIR(Catch=Catch, draws=draws, deplete.mean=2,
-                deplete.cv=0.5)
+fit <- run.SIR(nrep=nrep, Catch=Catch, Taxon=Taxon, InitialDepletePrior=.8,
+               InitialDepleteCV=1, deplete.mean=2, deplete.cv=0.5)
 
 ## Quick plot to show the output
 par(mfrow=c(3,1))
@@ -32,5 +28,9 @@ fit$years <- 2005:2015
 plot_ssb(fit)
 plot_bstatus(fit)
 plot_ustatus(fit)
+
 ## plots of MSY reference points and NLL
 plot_reference(fit)
+
+## Look at prior vs posterior patterns
+plot_draws(fit)
